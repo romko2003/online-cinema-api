93697c22ccc279a3f98c02b4c2114bd853cbf0c5
= <=>
2026-02-17 13:46:04 +0200

    feat: add movies domain models and migration

--- app/db/models/__init__.py
+++ app/db/models/__init__.py
@@ -6,12 +6,20 @@ from app.db.models.accounts import (
     UserGroup,
     UserProfile,
 )
+from app.db.models.movies import Certification, Director, Genre, Movie, Star
 
 __all__ = [
+    # accounts
     "User",
     "UserGroup",
     "UserProfile",
     "ActivationToken",
     "RefreshToken",
     "PasswordResetToken",
+    # movies
+    "Genre",
+    "Star",
+    "Director",
+    "Certification",
+    "Movie",
 ]
