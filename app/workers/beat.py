4cb4d3f47f21f6482c36884704d66fcd3467b330
= <=>
2026-02-17 13:38:55 +0200

    feat: extend celery cleanup tasks for password reset tokens

--- app/workers/beat.py
+++ app/workers/beat.py
@@ -4,5 +4,9 @@ celery_app.conf.beat_schedule = {
     "cleanup-activation-tokens-every-hour": {
         "task": "app.workers.tasks.accounts.cleanup_expired_activation_tokens",
         "schedule": 3600.0,
-    }
+    },
+    "cleanup-password-reset-tokens-every-hour": {
+        "task": "app.workers.tasks.accounts.cleanup_expired_password_reset_tokens",
+        "schedule": 3600.0,
+    },
 }
